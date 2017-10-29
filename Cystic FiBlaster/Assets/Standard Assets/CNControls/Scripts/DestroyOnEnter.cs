34300dd84d00f288b03e924531f5fe63d68cf3ad
Daniel Justice <danieljustice09@gmail.com>
2017-10-28 23:07:47 -0400

    killing the tiny dragons

---
+++ Cystic FiBlaster/Assets/Standard Assets/CNControls/Scripts/DestroyOnEnter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnEnter : MonoBehaviour {
+    //public Tag tagToDestroy;
+    public string tag;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.tag);
+        if (other.tag == tag)
+        {
+            print("DESTROY");
+            Destroy(other.gameObject);
+        }
+    }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    print("Something entered");
+    //    if (other.tag == tag)
+    //    {
+    //        Destroy(other.gameObject);
+    //    }
+    //}
+}
