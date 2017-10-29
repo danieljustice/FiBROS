8fc10bdfd198265047aabaf83c36808dbb0fe977
Daniel Justice <danieljustice09@gmail.com>
2017-10-29 04:06:07 -0400

    player takes damage and dies, things get destroyed

---
+++ Cystic FiBlaster/Assets/Standard Assets/CNControls/Scripts/DestroyParent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParent : MonoBehaviour {
+
+
+    private void OnDisable()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+}
